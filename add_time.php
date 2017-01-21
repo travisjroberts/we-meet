29ac6534f492dedb0fe5de1f8783ff0415decb87
SCROLLERMAN <SCROLLERMAN@users.noreply.github.com>
2017-01-21 09:45:20 -0600

    Added install.php file to create the database. Added necessary variables in the index.html file for adding a time. Created add_time.php as a placeholder for adding time.

---
+++ add_time.php
@@ -0,0 +1,9 @@
+<?php
+
+// Example query
+// INSERT INTO availability (time_stamp, year, week, hour, minutes, user_id) 
+// VALUES ('1481121000', 2016, 50, 8, 30, 1)
+
+// TODO: Clean input and write to database.db.
+
+?>
---
+++ install.php
@@ -0,0 +1,23 @@
+<?php
+    // Open the database (will create the file if it doesn't exist)
+    $database = new SQLite3("database.db"); 
+
+    // Begin Create
+    $query_string = "CREATE TABLE IF NOT EXISTS availability (
+                        time_stamp REAL NOT NULL,
+                        year INTEGER NOT NULL,
+                        week INTEGER NOT NULL,
+                        hour INTEGER NOT NULL,
+                        minutes INTEGER NOT NULL,
+                        user_id INTEGER NOT NULL,
+                        PRIMARY KEY (year, week, hour, minutes, user_id)
+                      );";
+
+    $database->query($query_string);
+    echo "Database created";
+    // End Create
+
+
+    // Close the database
+    $database->close();
+?>
